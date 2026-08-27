538345acfda51b077167cb002ab73e2636141b50
gtmabhi007 <gtmabhi007@gmail.com>
2026-08-27 10:27:13 +0530

    Update practic set 3.3.py

--- python work classroom/practic set 3.3.py
+++ python work classroom/practic set 3.3.py
@@ -8,26 +8,26 @@ print("data type",type(a))
 # 2. Write a Python program to print the following slices of a string:
 # * First 5 characters * Last 5 characters * Characters from index 3 to 10
 # * Every second character * Reverse of the string
-# a = input("enter string:=")
-# print("first five character of string",a[:5:])
-# print("last five character",a[-5:])
-# print("index from 3 to 10",a[3:11:])
-# print("every second character",a[::2])
-# print("reverse of the string",a[::-1])
+a = input("enter string:=")
+print("first five character of string",a[:5:])
+print("last five character",a[-5:])
+print("index from 3 to 10",a[3:11:])
+print("every second character",a[::2])
+print("reverse of the string",a[::-1])
 
 #3.
 
 #4. Write a Python program to check whether a given string is a palindrome or not.
-# a = input("enter the string:=")
-# if a==a[::-1]:
-#   print("given string is palindrome")
-# else:
-#   print("given string not palindrome")
+a = input("enter the string:=")
+if a==a[::-1]:
+  print("given string is palindrome")
+else:
+  print("given string not palindrome")
 
 # 5. Write a Python program to replace every space with a hyphen (`-`) using both:
-# a = "I live in ghaziabad"
-# b = a.replace(" ", "-")
-# print("replace every space",b)
+a = "I live in ghaziabad"
+b = a.replace(" ", "-")
+print("replace every space",b)
 
 # 6. Write a Python program to check whether two strings are anagrams.
 
@@ -36,23 +36,23 @@ print("data type",type(a))
 
 
 # 8. Write a Python program to input a sentence and display every word on a new line.
-# a = input("enter your sentence:= ")
-# b = a.replace(" ","\n")
-# print("display every words in next line",b)
+a = input("enter your sentence:= ")
+b = a.replace(" ","\n")
+print("display every words in next line",b)
 
 # 9. Write a Python program to reverse the order of words in a sentence.
-# a = ("my name is abhishek gautam ") 
-# print(a[::-1])
+a = ("my name is abhishek gautam ") 
+print(a[::-1])
 
 # 10. Write a Python program to remove duplicate characters from a string.
 
 
 # 11. Write a Python program to find the longest word in a sentence.
-# a = input("Enter a sentence:=  ")
-# b = a.split()
-# longest_word = max(b, key=len)
-# print("longest word is:", longest_word)
-# print("length of word:", len(longest_word))
+a = input("Enter a sentence:=  ")
+b = a.split()
+longest_word = max(b, key=len)
+print("longest word is:", longest_word)
+print("length of word:", len(longest_word))
 
 # 12. Write a Python program that performs the following operations on a given string:
 # * Convert to uppercase * Convert to lowercase * Swap case
@@ -60,24 +60,24 @@ print("data type",type(a))
 # * Split into words * Join the words using a hyphen (`-`)
 
 
-# a = input("Enter a string value:= ")
+a = input("Enter a string value:= ")
 
-# print("convert into Uppercase:=", a.upper())
+print("convert into Uppercase:=", a.upper())
 
-# print("convert into Lowercase:=", a.lower())
+print("convert into Lowercase:=", a.lower())
 
-# print("convert into Swap case:=", a.swapcase())
+print("convert into Swap case:=", a.swapcase())
 
-# print("convert into Trimmed:=", a.strip())
+print("convert into Trimmed:=", a.strip())
 
-# b = a.replace("Python", "Java")
-# print("Replaced the value is:=", b)
+b = a.replace("Python", "Java")
+print("Replaced the value is:=", b)
 
-# words = a.split()
-# print("convert Split into words:", words)
+words = a.split()
+print("convert Split into words:", words)
 
-# h = "-".join(words)
-# print("Joined with hyphen:", h)
+h = "-".join(words)
+print("Joined with hyphen:", h)
 
 
 
