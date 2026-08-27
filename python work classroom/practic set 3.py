08550a57459825de0ff65047f2e8febebd365e94
gtmabhi007 <gtmabhi007@gmail.com>
2026-08-27 10:30:17 +0530

    Update practic set 3.py

--- python work classroom/practic set 3.py
+++ python work classroom/practic set 3.py
@@ -1,133 +1,133 @@
-# # Q-1 enter two value and type its type 
-# a = 2
-# b = 1.3
+ # Q-1 enter two value and type its type 
+a = 2
+b = 1.3
 
-# print (type (a))
-# print (type (b))
+print (type (a))
+print (type (b))
 
 # Q-2 enter two integers and apply all the arithmetic operations
 
-# a = int(input("Enter first number: "))  
-# b = int(input("Enter second number: ")) 
+a = int(input("Enter first number: "))  
+b = int(input("Enter second number: ")) 
 
-# print("Addition:", a + b)
-# print("Subtraction:", a - b)
-# print("Multiplication:", a * b)
-# print("Division:", a / b)
-# print("Modulus:", a % b)
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Division:", a / b)
+print("Modulus:", a % b)
 
 # Q-3 
-# a = 5
+a = 5
 
-# float_a = float(a)
-# print(float_a) 
+float_a = float(a)
+print(float_a) 
 
-# complex_a = complex(a)
-# print(complex_a)  
+complex_a = complex(a)
+print(complex_a)  
 
 
 # Q-4 the complex
-# a = 5 + 4j
-# b = complex(2,3)
+a = 5 + 4j
+b = complex(2,3)
 
-# print (a.real)
-# print (a.imag)
+print (a.real)
+print (a.imag)
 
-# print (b.real)
-# print (b.imag)
+print (b.real)
+print (b.imag)
 
 
 # Q - 5
-# a = float (input("enter the first value"))
-# b = float (input("enter the second value"))
-# c = float (input("enter the third value"))
+a = float (input("enter the first value"))
+b = float (input("enter the second value"))
+c = float (input("enter the third value"))
 
-# d = (a+b+c) / 3 
+d = (a+b+c) / 3 
 
-# print (" The value of the average of the three number which are in the float is : " , d )
+print (" The value of the average of the three number which are in the float is : " , d )
 
 
 # # Q - 6 
-# a = 12 
-# b = 12.5
-# c = 4 + 5j 
+a = 12 
+b = 12.5
+c = 4 + 5j 
 
-# d = a + b 
-# print ("The additon of a and b is :", d)
-# print (type(d))
+d = a + b 
+print ("The additon of a and b is :", d)
+print (type(d))
 
-# e = b + c 
-# print ("The addition of b and c is : ", e )
-# print (type (e))
+e = b + c 
+print ("The addition of b and c is : ", e )
+print (type (e))
 
-# f = a + c 
-# print ("The addition of a and c is : ", f )
-# print (type (f))
+f = a + c 
+print ("The addition of a and c is : ", f )
+print (type (f))
 
 
-# Q - 7 
-# a = int (input ("Enter the value of the integer"))
-# b = float (input ("Enter the value of the float"))
-# c= complex (input ("Enter the value of the complex"))
+Q - 7 
+a = int (input ("Enter the value of the integer"))
+b = float (input ("Enter the value of the float"))
+c= complex (input ("Enter the value of the complex"))
 
-# print (a)
-# print (b)
-# print (c)
+print (a)
+print (b)
+print (c)
 
-# print (type (a))
-# print (type (b))
-# print (type (c))
+print (type (a))
+print (type (b))
+print (type (c))
 
 # # Q-8 
-# basic_salary = 58750
-# hra = basic_salary * 0.22
-# da = basic_salary * 0.15
-# professional_tax = 2500
+basic_salary = 58750
+hra = basic_salary * 0.22
+da = basic_salary * 0.15
+professional_tax = 2500
 
-# gross_salary = basic_salary + hra + da 
-# net_salary = gross_salary - professional_tax
-# rounded_net_salary = round (net_salary, 2)
+gross_salary = basic_salary + hra + da 
+net_salary = gross_salary - professional_tax
+rounded_net_salary = round (net_salary, 2)
  
-# print ( "the gross salary is ",gross_salary)
-# print (  "the net salary is ",net_salary)
-# print ( "hra is ",hra)
-# print ("da is ",da)
+print ( "the gross salary is ",gross_salary)
+print (  "the net salary is ",net_salary)
+print ( "hra is ",hra)
+print ("da is ",da)
 
-# print ( type (net_salary))
+print ( type (net_salary))
 
 # Q- 9 
 
-# A = 245
-# B = 37 
-# C = -128.75
+A = 245
+B = 37 
+C = -128.75
 
-# print("the A has a power of 2 is :",pow(A,2))
-# print ("The absolute value of c is :",abs(c))
+print("the A has a power of 2 is :",pow(A,2))
+print ("The absolute value of c is :",abs(c))
 
-# d = (A+B+C)/3
-# print("The average of the three value is :", d)
+d = (A+B+C)/3
+print("The average of the three value is :", d)
 
 # # Q - 10 
 
-# c1 = 6 +9j 
-# c2 = 4 - 7j 
+c1 = 6 +9j 
+c2 = 4 - 7j 
 
-# print ("the addition of  the two complex number with absolute value  is :", abs(c1 + c2) )
-# print ("the multiplication of the two complex number with absolute value is :", abs(c1 * c2) )
+print ("the addition of  the two complex number with absolute value  is :", abs(c1 + c2) )
+print ("the multiplication of the two complex number with absolute value is :", abs(c1 * c2) )
 
-# print ("the data type of the addition result of the absolute complex number is :",type (abs(c1+c2)))
-# print ("The data type of the multiplication result of the absolute complex number is :",type (abs(c1*c2)))
+print ("the data type of the addition result of the absolute complex number is :",type (abs(c1+c2)))
+print ("The data type of the multiplication result of the absolute complex number is :",type (abs(c1*c2)))
 
-# print ("The address of the addition result of the absolute complex number is :",id(abs(c1+c2)))
-# print ("The address type of the multiplication result of the absolute complex number is :",id(abs(c1*c2)))
+print ("The address of the addition result of the absolute complex number is :",id(abs(c1+c2)))
+print ("The address type of the multiplication result of the absolute complex number is :",id(abs(c1*c2)))
 
 #  #Q - 11 
-# print("-" * 36)
-# print("          STUDENT PROFILE")
-# print("-" * 36)
-# print("Name       : Rahul Sharma")
-# print("Age        : 20")
-# print("Course     : B.Tech")
-# print("University : ABC University")
-# print("City       : Delhi")
-# print("-" * 36)
+print("-" * 36)
+print("          STUDENT PROFILE")
+print("-" * 36)
+print("Name       : Rahul Sharma")
+print("Age        : 20")
+print("Course     : B.Tech")
+print("University : ABC University")
+print("City       : Delhi")
+print("-" * 36)
