2bb5cdc4cb80a5618ea2d47e714ca8a910be8651
gtmabhi007 <gtmabhi007@gmail.com>
2026-08-27 10:22:08 +0530

    Update practic set 2.py

--- python work classroom/practic set 2.py
+++ python work classroom/practic set 2.py
@@ -1,146 +1,145 @@
 # q1= write a programe to input two integer and display:addition ,subtraction, multiplication 
 # ,division,floor division,modulus and exponentiation.
 
-# a=int(input("enter the first number:-"))
-# b=int(input("enter the second number:-"))
-# print("addition :",a+b)
-# print("subtraction :",a-b)
-# print("multiplication",a*b)
-# print("division",a/b)
-# print("floor division",a//b)
-# print("modulus",a%b)
-# print("exponentation",a**b)
+a=int(input("enter the first number:-"))
+b=int(input("enter the second number:-"))
+print("addition :",a+b)
+print("subtraction :",a-b)
+print("multiplication",a*b)
+print("division",a/b)
+print("floor division",a//b)
+print("modulus",a%b)
+print("exponentation",a**b)
 
-#  Q-2 Take an integer as input from the user
-# a = int(input("Enter an integer:-"))
-# a += 10
-# print(a)
-# a -= 5
-# print(a)
-# a *= 2
-# print(a)
-# a /= 3
-# print(a)
+ # Q-2 Take an integer as input from the user
+a = int(input("Enter an integer:-"))
+a += 10
+print(a)
+a -= 5
+print(a)
+a *= 2
+print(a)
+a /= 3
+print(a)
 
-# Q-3 
-# Prompt the user to input two numbers
-# num1 = float(input("Enter the first number: "))
-# num2 = float(input("Enter the second number: "))
+# Q-3 Prompt the user to input two numbers
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
 
-# if num1 == num2:
-#     print("Are they equal? Yes")
-# else:
-#     print("Are they equal? No")
+if num1 == num2:
+    print("Are they equal? Yes")
+else:
+    print("Are they equal? No")
 
-# if num1 != num2:
-#     print("Are they not equal? Yes")
-# else:
-#     print("Are they not equal? No")
+if num1 != num2:
+    print("Are they not equal? Yes")
+else:
+    print("Are they not equal? No")
 
-# if num1 > num2:
-#     print("Is the first number greater than the second? Yes")
-# else:
-#     print("Is the first number greater than the second? No")
+if num1 > num2:
+    print("Is the first number greater than the second? Yes")
+else:
+    print("Is the first number greater than the second? No")
 
-# if num1 <= num2:
-#     print("Is the first number less than or equal to the second?  Yes")
-# else:
-#     print("Is the first number less than or equal to the second? No")
+if num1 <= num2:
+    print("Is the first number less than or equal to the second?  Yes")
+else:
+    print("Is the first number less than or equal to the second? No")
 
 
 # Q-4 Input marks for two subjects
-# subject1 = float(input("Enter marks Subject 1: "))
-# subject2 = float(input("Enter marks Subject 2: "))
+subject1 = float(input("Enter marks Subject 1: "))
+subject2 = float(input("Enter marks Subject 2: "))
 
 
-# if (subject1 >= 35) and (subject2 >= 35) :
-#     print("Pass")
+if (subject1 >= 35) and (subject2 >= 35) :
+    print("Pass")
 
-# if (subject1 >= 90) or (subject2 >= 90) :
-#     print("Eligible for Scholarship")
+if (subject1 >= 90) or (subject2 >= 90) :
+    print("Eligible for Scholarship")
 
-# if not (subject1 < 35 or subject2 < 35):
-#     print("Student has NOT failed")
-# else:
-#     print("Student has failed")
+if not (subject1 < 35 or subject2 < 35):
+    print("Student has NOT failed")
+else:
+    print("Student has failed")
 
 # Q-5  enter the input sentence and word . check  if word is in the sentence.
-# sentence = input ("enter the sentence")
-# word = input ("enter the word to check it in the sentence")
+sentence = input ("enter the sentence")
+word = input ("enter the word to check it in the sentence")
 
 
-# if word in sentence:
-#     print(word," is present in the sentence.")
-# else:
-#     print(word , " is not present in the sentence.")
+if word in sentence:
+    print(word," is present in the sentence.")
+else:
+    print(word , " is not present in the sentence.")
 
-#  Q-6 enter the input sentence and word . check  if word is in the sentence.
-# sentence = input ("enter the sentence")
-# word = input ("enter the word to check it in the sentence")
+ # Q-6 enter the input sentence and word . check  if word is in the sentence.
+sentence = input ("enter the sentence")
+word = input ("enter the word to check it in the sentence")
 
 
-# if word not in sentence:
-#     print(word," is present in the sentence.")
-# else:
-#     print(word , " is not present in the sentence.")
+if word not in sentence:
+    print(word," is present in the sentence.")
+else:
+    print(word , " is not present in the sentence.")
 
 # Q -7 , Q-8 Check whether a is b and a is equal to c.
-# a = input ("enter the value of a ")
-# b = input ("enter the value of b ")
-# c = input ("enter the value of c ")
-# if  (a == b) :
-#    print("a is equal to b")
-# else :
-#     print ("a is not equal to b")
+a = input ("enter the value of a ")
+b = input ("enter the value of b ")
+c = input ("enter the value of c ")
+if  (a == b) :
+   print("a is equal to b")
+else :
+    print ("a is not equal to b")
 
-# C = input("enter the value of c")  
+C = input("enter the value of c")  
 
-# if (a == C) :
-#     print("a is equal to c")
-# else :
-#     print ("a is not equal to c")
+if (a == C) :
+    print("a is equal to c")
+else :
+    print ("a is not equal to c")
 
-#  Q-9  Take two integers as input from the user and do the bitwise (&) and do the bitwise (or).
-# num1 = int(input("Enter first integer: "))
-# num2 = int(input("Enter second integer: "))
-
-
-# bitwise_and = num1 & num2
-# bitwise_or = num1 | num2
+ # Q-9  Take two integers as input from the user and do the bitwise (&) and do the bitwise (or).
+num1 = int(input("Enter first integer: "))
+num2 = int(input("Enter second integer: "))
 
 
-# print("the bitwise AND is ", bitwise_and)
-# print("the bitwise OR is" , bitwise_or)
+bitwise_and = num1 & num2
+bitwise_or = num1 | num2
+
+
+print("the bitwise AND is ", bitwise_and)
+print("the bitwise OR is" , bitwise_or)
 
 # Q- 10 Take two integers as input from the user and do the bitwise (XOR) and do the bitwise (NOT).
-# num1 = int(input("Enter first integer: "))
-# num2 = int(input("Enter second integer: "))
+num1 = int(input("Enter first integer: "))
+num2 = int(input("Enter second integer: "))
 
 
-# bitwise_xor = num1 ^ num2
-# bitwise_not1 = ~num1 
-# bitwise_not2 = ~num2 
+bitwise_xor = num1 ^ num2
+bitwise_not1 = ~num1 
+bitwise_not2 = ~num2 
 
 
-# print("the bitwise XOR is ", bitwise_xor)
-# print("the bitwise NOT 1 is" , bitwise_not1)
-# print("the bitwise NOT 2 is" , bitwise_not2)
+print("the bitwise XOR is ", bitwise_xor)
+print("the bitwise NOT 1 is" , bitwise_not1)
+print("the bitwise NOT 2 is" , bitwise_not2)
 
-#  Q- 11 left shift and right shift.
-# num = int(input("Enter a number: "))
+ # Q- 11 left shift and right shift.
+num = int(input("Enter a number: "))
 
-# left_shift_result = num << 2
-# right_shift_result = num >> 2
+left_shift_result = num << 2
+right_shift_result = num >> 2
 
-# print("the left shift result is ",left_shift_result)
-# print("the right shift result is ", right_shift_result)
+print("the left shift result is ",left_shift_result)
+print("the right shift result is ", right_shift_result)
 
 # Q- 12  Input three numbers from the user
-# num1 = float(input("Enter the first number: "))
-# num2 = float(input("Enter the second number: "))
-# num3 = float(input("Enter the third number: "))
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+num3 = float(input("Enter the third number: "))
 
-# if (((num1 > num2) and (num3 > num1)) == 1 ) :
-#     print("the number 1 is greater than number 2 but less than number 3")
-# else:
-#     print("Either number 1 is not greater than number 2 or  number 1 is not less than number 3 and both could be the case ")
+if (((num1 > num2) and (num3 > num1)) == 1 ) :
+    print("the number 1 is greater than number 2 but less than number 3")
+else:
+    print("Either number 1 is not greater than number 2 or  number 1 is not less than number 3 and both could be the case ")
